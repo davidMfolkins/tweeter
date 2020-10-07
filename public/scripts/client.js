398b34726fd0216195fb56ad99894889502de260
David Folkins <david.folkins88@gmail.com>
2020-10-07 19:19:06 -0400

    Add error validation with some animation, move the functions to a helper function file

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,58 +5,12 @@
  */
 $(document).ready(function() {
 
-  const escape = (str) => {
-    let div = document.createElement("div");
-    div.appendChild(document.createTextNode(str));
-    return div.innerHTML
-   }
+  $("#new-tweet").click(function(){
+    console.log("working")
+    $('#tweet-text').focus();
+});
 
-  const createTweetElement = function(tweet) {
-    let $tweet = $(`
-    <article>
-    <header>
-      <div>
-        <p>
-          <img src="${tweet.user.avatars}">
-          <span>${tweet.user.name}</span>
-        </p> 
-        <span class="reveal">${tweet.user.handle}</span> 
-      </div>
-    </header>
-    <div class="user-tweet">
-      <p>${escape(tweet.content.text)}</p>
-    </div>
-    <footer>
-      <div>
-        <p>${new Date(tweet.created_at).toLocaleString()}</p>
-        <p class="icons">
-          <i class="fas fa-flag"></i> 
-          <i class="fas fa-retweet"></i> 
-          <i class="fas fa-heart"></i>
-        </p>
-      </div>
-    </footer>
-  </article>
-  `)
-  return $tweet
-  } 
-
-
-  const renderTweets = function(tweets) {
-    for (const item of tweets) {
-      const result = createTweetElement(item)
-      $(".tweets").prepend(result)
-    }
-  }
-
-  const loadTweets = function() {
-    $.ajax("/tweets", {method: "GET"})
-    .then((res) => {
-    $(".tweets").empty()
-    renderTweets(res)
-    })
-  }
-
+//Shows errors if condtions are met, otherwise it will accept user input and display a new tweet 
   $(function() {
     const $form = $("form")
     $form.on("submit", function (event) {
@@ -65,15 +19,17 @@ $(document).ready(function() {
       let content = $(this).children("textarea").val()
 
       if (content === "") {
-        let error = $("#error").append("Please input a tweet")
-        return error.slideDown()
+        showError("Please input a tweet")
+
 
       } else if (content.length > 140) {
-        $("#error").append("Your tweet is over 140 characters long")
+        showError("Your tweet is over 140 characters long")
 
       } else {
         $.ajax("/tweets", {method : "POST", data: serialized})
         .then(function() {
+          $("#error").empty()
+          $("#error").removeClass("warning")
           $("#counter").text("140") 
           $("#form")[0].reset()
           loadTweets()
